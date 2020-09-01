f5317758685ffa6a6ad42f09d6bd9291f803a31f
Nancy <iamshinan@gmail.com>
2020-09-01 11:26:09 +1000

    geocoder API setup

---
+++ middleware/error.js
@@ -0,0 +1,25 @@
+const ErrorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+  let error = {
+    ...err
+  }
+
+  error.message = err.message
+
+  //Log to console for dev
+  // console.log(err);
+
+  //Mongoose bad objectId
+  if (err.name == 'CastError') {
+    const message = `Bootcamp not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404)
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server Error'
+  });
+}
+
+module.exports = errorHandler;
