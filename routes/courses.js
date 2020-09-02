1ef1912f33e0d060ea5d9e790021c78f9333a169
Nancy <iamshinan@gmail.com>
2020-09-02 22:38:56 +1000

    delete course controller and route added

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,8 @@ const {
   getCourses,
   getCourse,
   addCourse,
-  updateCourse
+  updateCourse,
+  deleteCourse
 } = require('../controllers/courses');
 
 const router = express.Router({
@@ -15,7 +16,7 @@ route('/').
 get(getCourses).
 post(addCourse);
 
-router.route('/:id').get(getCourse).put(updateCourse)
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse)
 
 
 module.exports = router;
