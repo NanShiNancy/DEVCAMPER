cc76313d8e3186ffdb4de86a09b63a4ce64f507a
Nancy <iamshinan@gmail.com>
2020-09-05 22:52:48 +1000

    course ownership for course added update and delete setup

--- controllers/courses.js
+++ controllers/courses.js
@@ -52,11 +52,21 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
   //Reffer to model-course-bootcamp
   req.body.bootcamp = req.params.bootcampId;
 
+  req.body.user = req.user.id;
+
   const bootcamp = await Boocamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
     return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`),
       404)
+  }
+
+  // // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      //customised error response
+      new ErrorResponse(`User ${req.user.id} is not authorized to add a course to bootcamp${bootcamp._id}`, 401)
+    );
   }
 
   const course = await Course.create(req.body);
@@ -76,6 +86,14 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(new ErrorResponse(`No course with the id of ${req.params.id}`),
       404)
+  }
+
+  // // Make sure user is bootcamp owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      //customised error response
+      new ErrorResponse(`User ${req.user.id} is not authorized to update course ${course_.id} to bootcamp${bootcamp._id}`, 401)
+    );
   }
 
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
@@ -99,6 +117,15 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse(`No course with the id of ${req.params.id}`),
       404)
   }
+
+  // // Make sure user is bootcamp owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      //customised error response
+      new ErrorResponse(`User ${req.user.id} is not authorized to delete course ${course_.id} to bootcamp${bootcamp._id}`, 401)
+    );
+  }
+
 
   await course.remove();
 
--- models/course.js
+++ models/course.js
@@ -35,7 +35,13 @@ const CourseSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'Bootcamp',
     required: true
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
   }
+
 
 });
 
