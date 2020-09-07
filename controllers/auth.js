3a58f931e47d686da1b22dfa5027b502073ab515
Nancy <iamshinan@gmail.com>
2020-09-07 22:30:58 +1000

    forgotpassword send email setup using mailtrap

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
+const sendEmail = require("../utils/sendEmail");
 const User = require("../models/user");
 
 //@desc     Register new user
@@ -7,119 +8,139 @@ const User = require("../models/user");
 //@access   Public
 
 exports.register = asyncHandler(async (req, res, next) => {
-  const {
-    name,
-    email,
-    password,
-    role
-  } = req.body;
+	const { name, email, password, role } = req.body;
 
-  // Create user
-  const user = await User.create({
-    name,
-    email,
-    password,
-    role,
-  });
+	// Create user
+	const user = await User.create({
+		name,
+		email,
+		password,
+		role,
+	});
 
-  sendTokenResponse(user, 200, res);
+	sendTokenResponse(user, 200, res);
 });
-
 
 //@desc     User login
 //@route    POST /api/v1/auth/login
 //@access   Public
 
 exports.login = asyncHandler(async (req, res, next) => {
-  const {
-    email,
-    password
-  } = req.body;
+	const { email, password } = req.body;
 
-  // Validate email & password
-  if (!email || !password) {
-    return next(new ErrorResponse('Please provide en email and password', 400));
-  }
+	// Validate email & password
+	if (!email || !password) {
+		return next(new ErrorResponse("Please provide en email and password", 400));
+	}
 
-  // Check for user and add password to email
-  // In the model-user: password: {	select: false, }
+	// Check for user and add password to email
+	// In the model-user: password: {	select: false, }
 
-  const user = await User.findOne({
-    email
-  }).select('+password'); //now-----password: { select: password}
+	const user = await User.findOne({
+		email,
+	}).select("+password"); //now-----password: { select: password}
 
-  if (!user) {
-    return next(new ErrorResponse('Invalid credentials', 401));
-  }
+	if (!user) {
+		return next(new ErrorResponse("Invalid credentials", 401));
+	}
 
-  // Check if password matches
-  const isMatch = await user.matchPassword(password);
+	// Check if password matches
+	const isMatch = await user.matchPassword(password);
 
-  if (!isMatch) {
-    return next(new ErrorResponse('Invalid credentials', 401));
-  }
+	if (!isMatch) {
+		return next(new ErrorResponse("Invalid credentials", 401));
+	}
 
-  sendTokenResponse(user, 200, res);
+	sendTokenResponse(user, 200, res);
 });
-
-
 
 //@desc     Get current logged in user
 //@route    POST /api/v1/auth/me
 //@access   Private
 
 exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
+	const user = await User.findById(req.user.id);
 
-  res.status(200).json({
-    success: true,
-    data: user
-  });
-})
+	res.status(200).json({
+		success: true,
+		data: user,
+	});
+});
 
 //@desc     Forgot password
 //@route    POST /api/v1/auth/forgotpassword
 //@access   Public
 
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne({
-    email: req.body.email
-  });
+	const user = await User.findOne({
+		email: req.body.email,
+	});
 
-  if (!user) {
-    return next(new ErrorResponse('There is no user with that email', 404));
-  }
+	if (!user) {
+		return next(new ErrorResponse("There is no user with that email", 404));
+	}
 
-  // Get rest token 
-  const resetToken = user.getResetPasswordToken();
+	// Get rest token
+	const resetToken = user.getResetPasswordToken();
 
-  await user.save({
-    validateBeforeSave: false
-  });
+	await user.save({
+		validateBeforeSave: false,
+	});
 
-  res.status(200).json({
-    success: true,
-    data: user
-  });
-})
+	// Create reset url
+	const resetUrl = `${req.protocol}://${req.get(
+		"host"
+	)}/api/v1/resetpassword/${resetToken}`;
+
+	const message = `Youare receiving this email because you has requested te reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+	try {
+		await sendEmail({
+			email: user.email,
+			subject: "Password reset token",
+			message,
+		});
+
+		// res.status(200).json({
+		//   success: true,
+		//   data: 'Email sent'
+		// });
+	} catch (err) {
+		console.log(err);
+		user.resetPasswordToken = undefined;
+		user.resetPasswordExpire = undefined;
+
+		await user.save({
+			validateBeforeSave: false,
+		});
+
+		return next(new ErrorResponse("Email could not be sent", 500));
+	}
+
+	res.status(200).json({
+		success: true,
+		data: "Email sent",
+	});
+});
 
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
-  // Create token
-  const token = user.getSignedJwtToken();
+	// Create token
+	const token = user.getSignedJwtToken();
 
-  const options = {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000), //equals 30 days
-    httpOnly: true
-  };
+	const options = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+		), //equals 30 days
+		httpOnly: true,
+	};
 
-  if (process.env.NODE_ENV === 'production') {
-    options.secure = true;
-  }
+	if (process.env.NODE_ENV === "production") {
+		options.secure = true;
+	}
 
-  res.status(statusCode).cookie('token', token, options).json({
-    success: true,
-    token
-  });
-
-}
+	res.status(statusCode).cookie("token", token, options).json({
+		success: true,
+		token,
+	});
+};
---
+++ utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SMTP_EMAIL, // generated ethereal user
+      pass: process.env.SMTP_PASSWORD, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
+
+}
+
+module.exports = sendEmail;
